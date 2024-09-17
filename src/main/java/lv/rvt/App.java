fb1bfe8628460e9fabeff5c503dfedbd0c040c9f
m1rkk <mrakkus15@gmail.com>
2024-09-17 10:55:12 +0300

    Speeding ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your group:");
-        String group = scanner.nextLine();
-        System.out.println("Please enter your name:");
-        String name = scanner.nextLine();
-        System.out.println("Please enter your last name:");
-        String lastName = scanner.nextLine();
-        System.out.println("Hello" + name + lastName  + "from" + group + "group!");
+        System.out.println("Give speed:" );
+        int ticket = scanner.nextInt();
+        if(ticket >= 120){
+            System.out.println("Speeding ticket!");
+        }
+
+
+
 
     }
 }
