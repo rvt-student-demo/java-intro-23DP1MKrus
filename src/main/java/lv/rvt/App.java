0ced8a570b23197c05f53313832240b4443657c9
m1rkk <mrakkus15@gmail.com>
2024-09-17 12:27:07 +0300

    Greater number

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,9 +7,14 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give speed:" );
-        int ticket = scanner.nextInt();
-        if(ticket > 120){
-            System.out.println("Speeding ticket!");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if(num1 > num2){
+            System.out.println("Greater number is: "+num1);
+        }
+        else {
+            System.out.println("Greater number is: "+num2);
         }
 
 
