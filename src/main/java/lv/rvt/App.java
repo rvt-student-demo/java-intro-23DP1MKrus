ef722df6968e1b58174dce3e22e5623654f64119
m1rkk <mrakkus15@gmail.com>
2024-09-24 12:38:00 +0300

    Sum of sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,10 +7,12 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number:" );
-        int start = scanner.nextInt();
-        for (int i = start; i < 101; i++) {
-            System.out.println(i);
+        int end = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i < end+1; i++) {
+                sum = sum + i;
         }
+        System.out.println("Your sum: " + sum);
 
 
 
