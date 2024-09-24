fde102bb223a2c760ac813221bb7dbad5073c927
m1rkk <mrakkus15@gmail.com>
2024-09-24 08:59:51 +0300

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,24 +6,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give speed:" );
-        int mark = scanner.nextInt();
+        System.out.println("Enter value of gift:" );
+        int gift = scanner.nextInt();
 
-        if(mark < 0){
+        if(gift < 0){
             System.out.println("Impossible");
 
-        }else if(mark > 0 && mark < 50){
-            System.out.println("failed");
-        } else if (mark > 49 && mark < 60) {
-            System.out.println("1");
-        }else if (mark > 59 && mark < 70) {
-            System.out.println("2");
-        }else if (mark > 69 && mark < 80) {
-            System.out.println("3");
-        }else if (mark > 79 && mark < 90) {
-            System.out.println("4");
-        }else if (mark > 89 && mark < 100) {
-            System.out.println("5");
+        }else if(gift >= 5000 && gift <= 25000){
+            System.out.println(100+(gift-5000)*0.08);
+        } else if (gift > 25000 && gift <= 55000) {
+            System.out.println(1700+(gift-25000)*0.1);
+        }else if (gift > 55000 && gift <= 200000) {
+            System.out.println(4700+(gift-55000)*0.12);
+        }else if (gift > 200000 && gift <= 1000000) {
+            System.out.println(22100+(gift-200000)*0.15);
+        }else if (gift > 1000000) {
+            System.out.println(142100+(gift-1000000)*0.17);
+        }else{
+            System.out.println("No tax!");
         }
 
 
