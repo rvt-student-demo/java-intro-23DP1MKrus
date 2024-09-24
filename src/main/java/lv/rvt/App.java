51baba7c52c218414c71f9983b75f40efc98193f
m1rkk <mrakkus15@gmail.com>
2024-09-24 08:45:41 +0300

    Greater number

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,16 +7,24 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give speed:" );
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
+        int mark = scanner.nextInt();
 
-        if(num1 > num2){
-            System.out.println("Greater number is: "+num1);
-        }
-        else {
-            System.out.println("Greater number is: "+num2);
-        }
+        if(mark < 0){
+            System.out.println("Impossible");
 
+        }else if(mark > 0 && mark < 50){
+            System.out.println("failed");
+        } else if (mark > 49 && mark < 60) {
+            System.out.println("1");
+        }else if (mark > 59 && mark < 70) {
+            System.out.println("2");
+        }else if (mark > 69 && mark < 80) {
+            System.out.println("3");
+        }else if (mark > 79 && mark < 90) {
+            System.out.println("4");
+        }else if (mark > 89 && mark < 100) {
+            System.out.println("5");
+        }
 
 
 
