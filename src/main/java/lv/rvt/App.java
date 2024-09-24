dea12e146de9c0db35d44638e93fd6bb270c428c
m1rkk <mrakkus15@gmail.com>
2024-09-24 12:25:33 +0300

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,24 +6,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter value of gift:" );
-        int gift = scanner.nextInt();
-
-        if(gift < 0){
-            System.out.println("Impossible");
-
-        }else if(gift >= 5000 && gift <= 25000){
-            System.out.println(100+(gift-5000)*0.08);
-        } else if (gift > 25000 && gift <= 55000) {
-            System.out.println(1700+(gift-25000)*0.1);
-        }else if (gift > 55000 && gift <= 200000) {
-            System.out.println(4700+(gift-55000)*0.12);
-        }else if (gift > 200000 && gift <= 1000000) {
-            System.out.println(22100+(gift-200000)*0.15);
-        }else if (gift > 1000000) {
-            System.out.println(142100+(gift-1000000)*0.17);
-        }else{
-            System.out.println("No tax!");
+        System.out.println("Enter number:" );
+        int start = scanner.nextInt();
+        for (int i = start; i < 101; i++) {
+            System.out.println(i);
         }
 
 
