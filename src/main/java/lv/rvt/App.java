7a05a1c55eaadda8f9da081c69097eb45be340c7
m1rkk <mrakkus15@gmail.com>
2024-10-01 11:16:47 +0300

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        printText("In a hole in the ground there lived a method",7);
+        printUntilNumber(7);
 
 
 
     }
-    public static void printText(String text,int number){
-        for (int i = 0; i < number; i++) {
-            System.out.println(text);
+    public static void printUntilNumber(int number){
+        for (int i = 1; i < number+1; i++) {
+            System.out.println(i);
         }
 
     }
