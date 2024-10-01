b5c758f2777aa2aa898e541fa6759f2f3705200d
m1rkk <mrakkus15@gmail.com>
2024-10-01 10:49:05 +0300

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,33 +6,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number:" );
-        int end = scanner.nextInt();
-        int sum = 0;
-        int count = 0;
-        int odd = 0;
-        int even = 0;
-        while (end != -1){
-            count++;
-            sum += end;
-            end = scanner.nextInt();
-            if(end%2==0){
-                even++;
-            }
-            else {
-                odd++;
-            }
+        printText("In a hole in the ground there lived a method",7);
+
+
+
+    }
+    public static void printText(String text,int number){
+        for (int i = 0; i < number; i++) {
+            System.out.println(text);
         }
-        double avg = (double) sum / count;
-        System.out.println("Thx!Bye!");
-        System.out.println("Your sum: " + sum);
-        System.out.println("Count of nums: " + count);
-        System.out.println("Your avg:" + avg);
-        System.out.println("Odd: " + odd);
-        System.out.println("Even: " + even);
-
-
-
 
     }
 }
