7e2f3d6ac4e4b53857f0107d278a3e13cd9897ee
m1rkk <mrakkus15@gmail.com>
2024-10-08 10:26:30 +0300

    Stars part 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,10 +10,19 @@ public class App {
         }
         System.out.println("");
     }
+    public static void printSquare(int number) {
+        for (int i = 0; i < number; i++) {
+            printStars(number);
+
+
+        }
+
+    }
     public static void main(String[] args) {
 
-    printStars(5);
-    printStars(5);
+//    printStars(5);
+//    printStars(5);
+        printSquare(4);
 
 
     }
